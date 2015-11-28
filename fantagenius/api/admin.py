502015d0060005dd3e5a703297779304b0776d60
Giacomo Bresciani <brescia123@gmail.com>
2015-11-29 00:32:07 +0100

    added admin page

--- fantagenius/api/admin.py
+++ fantagenius/api/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from models import Team, Player, Vote
 
-# Register your models here.
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created', 'modified')
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'role', 'team', 'price', 'attendances', 'gol',
+                    'assist', 'yellow_cards', 'red_cards', 'penalties_kicked',
+                    'penalties_scored', 'penalties_missed', 'penalties_saved',
+                    'vote_avg', 'magicvote_avg', 'seriea', 'created',
+                    'modified')
+
+
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ('player', 'vote', 'gol', 'assist',
+                    'penalties_scored_saved', 'penalties_missed', 'own_gol',
+                    'yellow_cards', 'red_cards', 'magicvote', 'day', 'sub_in',
+                    'sub_out', 'created', 'modified')
+
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Vote, VoteAdmin)
--- fantagenius/urls.py
+++ fantagenius/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import include, url
+from django.contrib import admin
 from rest_framework import routers
 from fantagenius.api import views
 
@@ -10,4 +11,5 @@ router.register(r'vote', views.VoteViewSet)
 urlpatterns = [
     url(r'update', views.update_db),
     url(r'^', include(router.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 ]
